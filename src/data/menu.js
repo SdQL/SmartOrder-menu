fd1904c0ae62ea3b6a4e751d4567319d87ec408e
SDC <99742065+SdQL@users.noreply.github.com>
2025-06-20 00:54:29 -0500

    feat: add menu data file and clean up astro.config

---
+++ src/data/menu.js
@@ -0,0 +1,202 @@
+[
+  {
+    "id": 1,
+    "name": "Arepa Rellena",
+    "img": "https://i.postimg.cc/Wzv9y5gS/dd2b73b8-7420-4e42-9107-e5045d0ffb94.png",
+    "ingredients": ["harina de maíz", "queso", "mantequilla", "carne desmechada", "aguacate"],
+    "category": "fast-food",
+    "price": 12000
+  },
+  {
+    "id": 2,
+    "name": "Bandeja Paisa",
+    "img": "https://recetasdecocina.elmundo.es/wp-content/uploads/2025/05/bandeja-paisa-1024x683.jpg",
+    "ingredients": ["arroz", "fríjoles", "huevo frito", "chicharrón", "plátano maduro", "aguacate", "carne molida", "chorizo", "arepa"],
+    "category": "main-course",
+    "price": 32000
+  },
+  {
+    "id": 3,
+    "name": "Empanadas Colombianas",
+    "img": "https://www.campi.com.co/wp-content/uploads/2020/08/empanadas-colombianas-imagen-destacada.jpg",
+    "ingredients": ["masa de maíz", "papa", "carne molida", "cebolla", "ajo", "comino"],
+    "category": "fast-food",
+    "price": 7000
+  },
+  {
+    "id": 4,
+    "name": "Ajiaco",
+    "img": "https://i0.wp.com/surtidoradeaves.com/wp-content/uploads/2020/05/Ajiaco-Bogotano.png?fit=750%2C500&ssl=1",
+    "ingredients": ["pollo", "papas", "mazorca", "alcaparras", "crema de leche", "guascas"],
+    "category": "main-course",
+    "price": 28000
+  },
+  {
+    "id": 5,
+    "name": "Tamales Tolimenses",
+    "img": "https://i.ytimg.com/vi/LshM0mXlG-A/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLA_7DcWpcDcUTlacMvc-WnTCPn_pA",
+    "ingredients": ["masa de maíz", "pollo", "cerdo", "huevo", "zanahoria", "arvejas"],
+    "category": "main-course",
+    "price": 30000
+  },
+  {
+    "id": 6,
+    "name": "Arepa de Huevo",
+    "img": "https://chokliarepas.com/wp-content/uploads/2024/01/0631828474525674cae737cbe9df156b.jpg",
+    "ingredients": ["harina de maíz", "huevo", "aceite", "sal"],
+    "category": "fast-food",
+    "price": 8000
+  },
+  {
+    "id": 7,
+    "name": "Cazuela de Mariscos",
+    "img": "https://i.pinimg.com/474x/e7/b9/5e/e7b95e89e570ed0625d07632ef294b35.jpg",
+    "ingredients": ["camarones", "pulpo", "pescado", "crema de leche", "vino blanco"],
+    "category": "main-course",
+    "price": 34000
+  },
+  {
+    "id": 8,
+    "name": "Aborrajado",
+    "img": "https://cloudfront-us-east-1.images.arcpublishing.com/elespectador/TW4ZVQ5JPZGBLC7ZUUX5PHMA64.jpg",
+    "ingredients": ["plátano maduro", "queso", "huevo", "harina"],
+    "category": "fast-food",
+    "price": 9000
+  },
+  {
+    "id": 9,
+    "name": "Postre de Natas",
+    "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlBzKhspaM1OcwLaZwKQFsTdwkCjRaJxKwzQa-NIAU9wAH6SxselAbeJ_d3yBpsVZKIiA&usqp=CAU",
+    "ingredients": ["leche", "azúcar", "huevos", "canela", "cáscara de limón"],
+    "category": "dessert",
+    "price": 7000
+  },
+  {
+    "id": 10,
+    "name": "Cholado",
+    "img": "https://i.ytimg.com/vi/8kyMcD-ekOg/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDl6VBUkX_8YZ40Lk-uWGYg15Jblg",
+    "ingredients": ["hielo raspado", "leche condensada", "coctel de frutas", "sirope", "coco rallado"],
+    "category": "dessert",
+    "price": 8000
+  },
+  {
+    "id": 11,
+    "name": "Manjar Blanco",
+    "img": "https://comopreparar.co/wp-content/uploads/2024/06/Manjar-blanco-480x270.webp",
+    "ingredients": ["leche", "azúcar", "bicarbonato", "canela"],
+    "category": "dessert",
+    "price": 6000
+  },
+  {
+    "id": 12,
+    "name": "Lulada",
+    "img": "https://vecinavegetariana.com/wp-content/uploads/2022/05/Lulada-Lulo-Drink-Bebida-de-Lulo-Naranjilla-2-1-500x500.jpg",
+    "ingredients": ["lulo", "jugo de limón", "azúcar", "hielo", "agua"],
+    "category": "drink",
+    "price": 5000
+  },
+  {
+    "id": 13,
+    "name": "Refajo",
+    "img": "https://recetasdecolombia.com/wp-content/uploads/2022/02/Receta-para-hacer-refajo-colombiano-en-casa-e1645641111582-768x488.jpg",
+    "ingredients": ["gaseosa colombiana", "cerveza", "hielo"],
+    "category": "drink",
+    "price": 7000
+  },
+  {
+    "id": 14,
+    "name": "Café Colombiano",
+    "img": "https://blog.coffeemais.com/wp-content/uploads/2022/06/cafe-colombiano-interna.jpg",
+    "ingredients": ["café", "agua", "azúcar"],
+    "category": "drink",
+    "price": 4000
+  },
+  {
+    "id": 15,
+    "name": "Buñuelos",
+    "img": "https://www.elespectador.com/resizer/v2/DLEQPHYNTZFGVEPDJ2VQDANSDI.jpg?auth=d0fdad70a96e669e4afca2d38102d0762eab438fab7847070e5fcf3b014fb25e&width=920&height=613&smart=true&quality=60",
+    "ingredients": ["queso costeño", "harina", "huevo", "polvo de hornear"],
+    "category": "dessert",
+    "price": 6000
+  },
+  {
+    "id": 16,
+    "name": "Chicha",
+    "img": "https://www.recetaspanama.com/base/stock/Recipe/pesada-de-pina/pesada-de-pina_small.jpg.webp",
+    "ingredients": ["maíz fermentado", "agua", "panela"],
+    "category": "drink",
+    "price": 4000
+  },
+  {
+    "id": 17,
+    "name": "Mazamorra",
+    "img": "https://www.terra.com.co/u/fotografias/m/2024/6/14/f608x342-30101_59824_5050.jpg",
+    "ingredients": ["maíz pelado", "leche", "panela"],
+    "category": "dessert",
+    "price": 5000
+  },
+  {
+    "id": 18,
+    "name": "Salpicón",
+    "img": "https://i.pinimg.com/736x/c3/9b/13/c39b13d5fff97500fe58bce064f1898e.jpg",
+    "ingredients": ["frutas picadas", "soda", "jarabe de frutas"],
+    "category": "drink",
+    "price": 6000
+  },
+  {
+    "id": 19,
+    "name": "Chicharrón con Arepa",
+    "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOh28nQAZ-qshi7CrMqBAUYvi7_qCYMuttFA&s",
+    "ingredients": ["chicharrón", "sal", "arepa"],
+    "category": "main-course",
+    "price": 25000
+  },
+  {
+    "id": 20,
+    "name": "Pandebono con Bocadillo",
+    "img": "https://pbs.twimg.com/media/EYjbIaxXYAAw7CS.jpg",
+    "ingredients": ["queso", "harina de yuca", "huevo", "bocadillo"],
+    "category": "fast-food",
+    "price": 9000
+  },
+  {
+    "id": 21,
+    "name": "Pan de Bono",
+    "img": "https://i0.wp.com/anandaweb.com/wp-content/uploads/2019/04/pandebono.jpg?fit=1500%2C1500&ssl=1",
+    "ingredients": ["fécula de yuca", "queso", "huevos", "leche"],
+    "category": "fast-food",
+    "price": 7000
+  },
+  {
+    "id": 22,
+    "name": "Cuchuco de Trigo",
+    "img": "https://comedera.com/wp-content/uploads/sites/9/2021/11/cuchuco-colombiano.jpg?w=4096",
+    "ingredients": ["trigo", "papa", "zanahoria", "costilla de cerdo"],
+    "category": "main-course",
+    "price": 26000
+  },
+  {
+    "id": 23,
+    "name": "Lechona",
+    "img": "https://cdn.colombia.com/gastronomia/2011/07/22/lechona-1476.jpg",
+    "ingredients": ["cerdo", "arroz", "arvejas", "especias"],
+    "category": "main-course",
+    "price": 30000
+  },
+  {
+    "id": 24,
+    "name": "Merengón",
+    "img": "https://convivimos.naranjax.com/subidas/Merengon_port-jpg.webp",
+    "ingredients": ["claras de huevo", "azúcar", "fresas", "crema batida"],
+    "category": "dessert",
+    "price": 7500
+  },
+  {
+    "id": 25,
+    "name": "Sancocho",
+    "img": "https://www.elespectador.com/resizer/8poRYWbGKM2Px7-tbb5CHYAP5kw=/arc-anglerfish-arc2-prod-elespectador/public/NPCOUP725ZBQBCLN6TJ5RUSHHI.jpg",
+    "ingredients": ["pollo", "papa", "yuca", "mazorca", "plátano"],
+    "category": "main-course",
+    "price": 29000
+  }
+]
