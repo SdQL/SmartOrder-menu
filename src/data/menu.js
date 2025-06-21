da8ca263d3bb7fed0cdb4d4b4a1db9aa93632568
SDC <99742065+SdQL@users.noreply.github.com>
2025-06-21 15:16:59 -0500

    feat: restructure menu data to include categories and detailed item descriptions

--- src/data/menu.js
+++ src/data/menu.js
@@ -1,202 +1,206 @@
 [
   {
-    "id": 1,
-    "name": "Arepa Rellena",
-    "img": "https://i.postimg.cc/Wzv9y5gS/dd2b73b8-7420-4e42-9107-e5045d0ffb94.png",
-    "ingredients": ["harina de maíz", "queso", "mantequilla", "carne desmechada", "aguacate"],
-    "category": "fast-food",
-    "price": 12000
+    "id": "fast-food",
+    "name": "Comidas Rápidas",
+    "items": [
+      {
+        "id": 1,
+        "name": "Arepa Rellena",
+        "description": "Arepa de maíz rellena con carne desmechada, queso y aguacate",
+        "price": 12000,
+        "image": "https://i.postimg.cc/Wzv9y5gS/dd2b73b8-7420-4e42-9107-e5045d0ffb94.png",
+        "popular": true,
+        "prepTime": "8-10 min"
+      },
+      {
+        "id": 2,
+        "name": "Empanadas Colombianas",
+        "description": "Empanadas crujientes rellenas de carne molida y papa",
+        "price": 7000,
+        "image": "https://www.campi.com.co/wp-content/uploads/2020/08/empanadas-colombianas-imagen-destacada.jpg",
+        "popular": true,
+        "prepTime": "5-7 min"
+      },
+      {
+        "id": 3,
+        "name": "Arepa de Huevo",
+        "description": "Arepa frita con huevo entero en su interior",
+        "price": 8000,
+        "image": "https://chokliarepas.com/wp-content/uploads/2024/01/0631828474525674cae737cbe9df156b.jpg",
+        "popular": false,
+        "prepTime": "6-8 min"
+      },
+      {
+        "id": 4,
+        "name": "Aborrajado",
+        "description": "Plátano maduro relleno de queso, rebozado y frito",
+        "price": 9000,
+        "image": "https://cloudfront-us-east-1.images.arcpublishing.com/elespectador/TW4ZVQ5JPZGBLC7ZUUX5PHMA64.jpg",
+        "popular": false,
+        "prepTime": "5-7 min"
+      },
+      {
+        "id": 5,
+        "name": "Pandebono con Bocadillo",
+        "description": "Panecillo de queso y yuca acompañado de bocadillo",
+        "price": 9000,
+        "image": "https://pbs.twimg.com/media/EYjbIaxXYAAw7CS.jpg",
+        "popular": true,
+        "prepTime": "4-6 min"
+      }
+    ]
   },
   {
-    "id": 2,
-    "name": "Bandeja Paisa",
-    "img": "https://recetasdecocina.elmundo.es/wp-content/uploads/2025/05/bandeja-paisa-1024x683.jpg",
-    "ingredients": ["arroz", "fríjoles", "huevo frito", "chicharrón", "plátano maduro", "aguacate", "carne molida", "chorizo", "arepa"],
-    "category": "main-course",
-    "price": 32000
+    "id": "main-course",
+    "name": "Platos Fuertes",
+    "items": [
+      {
+        "id": 6,
+        "name": "Bandeja Paisa",
+        "description": "Plato tradicional con arroz, fríjoles, carne molida, chicharrón, huevo, plátano y arepa",
+        "price": 32000,
+        "image": "https://recetasdecocina.elmundo.es/wp-content/uploads/2025/05/bandeja-paisa-1024x683.jpg",
+        "popular": true,
+        "prepTime": "20-25 min"
+      },
+      {
+        "id": 7,
+        "name": "Ajiaco",
+        "description": "Sopa bogotana con pollo, papa, mazorca, guascas y alcaparras",
+        "price": 28000,
+        "image": "https://i0.wp.com/surtidoradeaves.com/wp-content/uploads/2020/05/Ajiaco-Bogotano.png?fit=750%2C500&ssl=1",
+        "popular": true,
+        "prepTime": "15-20 min"
+      },
+      {
+        "id": 8,
+        "name": "Tamales Tolimenses",
+        "description": "Tamales envueltos en hoja con masa, carnes y vegetales",
+        "price": 30000,
+        "image": "https://i.ytimg.com/vi/LshM0mXlG-A/hq720.jpg",
+        "popular": false,
+        "prepTime": "30-40 min"
+      },
+      {
+        "id": 9,
+        "name": "Lechona",
+        "description": "Cerdo relleno de arroz y arvejas, horneado por horas",
+        "price": 30000,
+        "image": "https://cdn.colombia.com/gastronomia/2011/07/22/lechona-1476.jpg",
+        "popular": true,
+        "prepTime": "25-35 min"
+      },
+      {
+        "id": 10,
+        "name": "Cazuela de Mariscos",
+        "description": "Mariscos en crema con un toque de vino blanco",
+        "price": 34000,
+        "image": "https://i.pinimg.com/474x/e7/b9/5e/e7b95e89e570ed0625d07632ef294b35.jpg",
+        "popular": false,
+        "prepTime": "12-15 min"
+      }
+    ]
   },
   {
-    "id": 3,
-    "name": "Empanadas Colombianas",
-    "img": "https://www.campi.com.co/wp-content/uploads/2020/08/empanadas-colombianas-imagen-destacada.jpg",
-    "ingredients": ["masa de maíz", "papa", "carne molida", "cebolla", "ajo", "comino"],
-    "category": "fast-food",
-    "price": 7000
+    "id": "drinks",
+    "name": "Bebidas",
+    "items": [
+      {
+        "id": 11,
+        "name": "Lulada",
+        "description": "Refrescante bebida de lulo con limón, azúcar y hielo",
+        "price": 5000,
+        "image": "https://vecinavegetariana.com/wp-content/uploads/2022/05/Lulada-Lulo-Drink-Bebida-de-Lulo-Naranjilla-2-1-500x500.jpg",
+        "popular": true,
+        "prepTime": "2-3 min"
+      },
+      {
+        "id": 12,
+        "name": "Refajo",
+        "description": "Mezcla de cerveza y gaseosa colombiana con hielo",
+        "price": 7000,
+        "image": "https://recetasdecolombia.com/wp-content/uploads/2022/02/Receta-para-hacer-refajo-colombiano-en-casa-e1645641111582-768x488.jpg",
+        "popular": true,
+        "prepTime": "1 min"
+      },
+      {
+        "id": 13,
+        "name": "Café Colombiano",
+        "description": "Café tradicional preparado con amor",
+        "price": 4000,
+        "image": "https://blog.coffeemais.com/wp-content/uploads/2022/06/cafe-colombiano-interna.jpg",
+        "popular": false,
+        "prepTime": "2-3 min"
+      },
+      {
+        "id": 14,
+        "name": "Chicha",
+        "description": "Bebida fermentada de maíz dulce y refrescante",
+        "price": 4000,
+        "image": "https://www.recetaspanama.com/base/stock/Recipe/pesada-de-pina/pesada-de-pina_small.jpg.webp",
+        "popular": false,
+        "prepTime": "1 min"
+      },
+      {
+        "id": 15,
+        "name": "Salpicón",
+        "description": "Coctel de frutas con soda y jarabe dulce",
+        "price": 6000,
+        "image": "https://i.pinimg.com/736x/c3/9b/13/c39b13d5fff97500fe58bce064f1898e.jpg",
+        "popular": true,
+        "prepTime": "2-3 min"
+      }
+    ]
   },
   {
-    "id": 4,
-    "name": "Ajiaco",
-    "img": "https://i0.wp.com/surtidoradeaves.com/wp-content/uploads/2020/05/Ajiaco-Bogotano.png?fit=750%2C500&ssl=1",
-    "ingredients": ["pollo", "papas", "mazorca", "alcaparras", "crema de leche", "guascas"],
-    "category": "main-course",
-    "price": 28000
-  },
-  {
-    "id": 5,
-    "name": "Tamales Tolimenses",
-    "img": "https://i.ytimg.com/vi/LshM0mXlG-A/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLA_7DcWpcDcUTlacMvc-WnTCPn_pA",
-    "ingredients": ["masa de maíz", "pollo", "cerdo", "huevo", "zanahoria", "arvejas"],
-    "category": "main-course",
-    "price": 30000
-  },
-  {
-    "id": 6,
-    "name": "Arepa de Huevo",
-    "img": "https://chokliarepas.com/wp-content/uploads/2024/01/0631828474525674cae737cbe9df156b.jpg",
-    "ingredients": ["harina de maíz", "huevo", "aceite", "sal"],
-    "category": "fast-food",
-    "price": 8000
-  },
-  {
-    "id": 7,
-    "name": "Cazuela de Mariscos",
-    "img": "https://i.pinimg.com/474x/e7/b9/5e/e7b95e89e570ed0625d07632ef294b35.jpg",
-    "ingredients": ["camarones", "pulpo", "pescado", "crema de leche", "vino blanco"],
-    "category": "main-course",
-    "price": 34000
-  },
-  {
-    "id": 8,
-    "name": "Aborrajado",
-    "img": "https://cloudfront-us-east-1.images.arcpublishing.com/elespectador/TW4ZVQ5JPZGBLC7ZUUX5PHMA64.jpg",
-    "ingredients": ["plátano maduro", "queso", "huevo", "harina"],
-    "category": "fast-food",
-    "price": 9000
-  },
-  {
-    "id": 9,
-    "name": "Postre de Natas",
-    "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlBzKhspaM1OcwLaZwKQFsTdwkCjRaJxKwzQa-NIAU9wAH6SxselAbeJ_d3yBpsVZKIiA&usqp=CAU",
-    "ingredients": ["leche", "azúcar", "huevos", "canela", "cáscara de limón"],
-    "category": "dessert",
-    "price": 7000
-  },
-  {
-    "id": 10,
-    "name": "Cholado",
-    "img": "https://i.ytimg.com/vi/8kyMcD-ekOg/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDl6VBUkX_8YZ40Lk-uWGYg15Jblg",
-    "ingredients": ["hielo raspado", "leche condensada", "coctel de frutas", "sirope", "coco rallado"],
-    "category": "dessert",
-    "price": 8000
-  },
-  {
-    "id": 11,
-    "name": "Manjar Blanco",
-    "img": "https://comopreparar.co/wp-content/uploads/2024/06/Manjar-blanco-480x270.webp",
-    "ingredients": ["leche", "azúcar", "bicarbonato", "canela"],
-    "category": "dessert",
-    "price": 6000
-  },
-  {
-    "id": 12,
-    "name": "Lulada",
-    "img": "https://vecinavegetariana.com/wp-content/uploads/2022/05/Lulada-Lulo-Drink-Bebida-de-Lulo-Naranjilla-2-1-500x500.jpg",
-    "ingredients": ["lulo", "jugo de limón", "azúcar", "hielo", "agua"],
-    "category": "drink",
-    "price": 5000
-  },
-  {
-    "id": 13,
-    "name": "Refajo",
-    "img": "https://recetasdecolombia.com/wp-content/uploads/2022/02/Receta-para-hacer-refajo-colombiano-en-casa-e1645641111582-768x488.jpg",
-    "ingredients": ["gaseosa colombiana", "cerveza", "hielo"],
-    "category": "drink",
-    "price": 7000
-  },
-  {
-    "id": 14,
-    "name": "Café Colombiano",
-    "img": "https://blog.coffeemais.com/wp-content/uploads/2022/06/cafe-colombiano-interna.jpg",
-    "ingredients": ["café", "agua", "azúcar"],
-    "category": "drink",
-    "price": 4000
-  },
-  {
-    "id": 15,
-    "name": "Buñuelos",
-    "img": "https://www.elespectador.com/resizer/v2/DLEQPHYNTZFGVEPDJ2VQDANSDI.jpg?auth=d0fdad70a96e669e4afca2d38102d0762eab438fab7847070e5fcf3b014fb25e&width=920&height=613&smart=true&quality=60",
-    "ingredients": ["queso costeño", "harina", "huevo", "polvo de hornear"],
-    "category": "dessert",
-    "price": 6000
-  },
-  {
-    "id": 16,
-    "name": "Chicha",
-    "img": "https://www.recetaspanama.com/base/stock/Recipe/pesada-de-pina/pesada-de-pina_small.jpg.webp",
-    "ingredients": ["maíz fermentado", "agua", "panela"],
-    "category": "drink",
-    "price": 4000
-  },
-  {
-    "id": 17,
-    "name": "Mazamorra",
-    "img": "https://www.terra.com.co/u/fotografias/m/2024/6/14/f608x342-30101_59824_5050.jpg",
-    "ingredients": ["maíz pelado", "leche", "panela"],
-    "category": "dessert",
-    "price": 5000
-  },
-  {
-    "id": 18,
-    "name": "Salpicón",
-    "img": "https://i.pinimg.com/736x/c3/9b/13/c39b13d5fff97500fe58bce064f1898e.jpg",
-    "ingredients": ["frutas picadas", "soda", "jarabe de frutas"],
-    "category": "drink",
-    "price": 6000
-  },
-  {
-    "id": 19,
-    "name": "Chicharrón con Arepa",
-    "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOh28nQAZ-qshi7CrMqBAUYvi7_qCYMuttFA&s",
-    "ingredients": ["chicharrón", "sal", "arepa"],
-    "category": "main-course",
-    "price": 25000
-  },
-  {
-    "id": 20,
-    "name": "Pandebono con Bocadillo",
-    "img": "https://pbs.twimg.com/media/EYjbIaxXYAAw7CS.jpg",
-    "ingredients": ["queso", "harina de yuca", "huevo", "bocadillo"],
-    "category": "fast-food",
-    "price": 9000
-  },
-  {
-    "id": 21,
-    "name": "Pan de Bono",
-    "img": "https://i0.wp.com/anandaweb.com/wp-content/uploads/2019/04/pandebono.jpg?fit=1500%2C1500&ssl=1",
-    "ingredients": ["fécula de yuca", "queso", "huevos", "leche"],
-    "category": "fast-food",
-    "price": 7000
-  },
-  {
-    "id": 22,
-    "name": "Cuchuco de Trigo",
-    "img": "https://comedera.com/wp-content/uploads/sites/9/2021/11/cuchuco-colombiano.jpg?w=4096",
-    "ingredients": ["trigo", "papa", "zanahoria", "costilla de cerdo"],
-    "category": "main-course",
-    "price": 26000
-  },
-  {
-    "id": 23,
-    "name": "Lechona",
-    "img": "https://cdn.colombia.com/gastronomia/2011/07/22/lechona-1476.jpg",
-    "ingredients": ["cerdo", "arroz", "arvejas", "especias"],
-    "category": "main-course",
-    "price": 30000
-  },
-  {
-    "id": 24,
-    "name": "Merengón",
-    "img": "https://convivimos.naranjax.com/subidas/Merengon_port-jpg.webp",
-    "ingredients": ["claras de huevo", "azúcar", "fresas", "crema batida"],
-    "category": "dessert",
-    "price": 7500
-  },
-  {
-    "id": 25,
-    "name": "Sancocho",
-    "img": "https://www.elespectador.com/resizer/8poRYWbGKM2Px7-tbb5CHYAP5kw=/arc-anglerfish-arc2-prod-elespectador/public/NPCOUP725ZBQBCLN6TJ5RUSHHI.jpg",
-    "ingredients": ["pollo", "papa", "yuca", "mazorca", "plátano"],
-    "category": "main-course",
-    "price": 29000
+    "id": "desserts",
+    "name": "Postres",
+    "items": [
+      {
+        "id": 16,
+        "name": "Postre de Natas",
+        "description": "Postre lácteo tradicional con canela y limón",
+        "price": 7000,
+        "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlBzKhspaM1OcwLaZwKQFsTdwkCjRaJxKwzQa-NIAU9wAH6SxselAbeJ_d3yBpsVZKIiA&usqp=CAU",
+        "popular": false,
+        "prepTime": "3-5 min"
+      },
+      {
+        "id": 17,
+        "name": "Cholado",
+        "description": "Hielo raspado con frutas, sirope y leche condensada",
+        "price": 8000,
+        "image": "https://i.ytimg.com/vi/8kyMcD-ekOg/hq720.jpg",
+        "popular": true,
+        "prepTime": "5-7 min"
+      },
+      {
+        "id": 18,
+        "name": "Manjar Blanco",
+        "description": "Dulce de leche espeso con canela",
+        "price": 6000,
+        "image": "https://comopreparar.co/wp-content/uploads/2024/06/Manjar-blanco-480x270.webp",
+        "popular": false,
+        "prepTime": "3-4 min"
+      },
+      {
+        "id": 19,
+        "name": "Buñuelos",
+        "description": "Bolas de masa de queso fritas, crujientes por fuera",
+        "price": 6000,
+        "image": "https://www.elespectador.com/resizer/v2/DLEQPHYNTZFGVEPDJ2VQDANSDI.jpg",
+        "popular": true,
+        "prepTime": "3-5 min"
+      },
+      {
+        "id": 20,
+        "name": "Merengón",
+        "description": "Postre con base de merengue, fresas y crema",
+        "price": 7500,
+        "image": "https://convivimos.naranjax.com/subidas/Merengon_port-jpg.webp",
+        "popular": true,
+        "prepTime": "4-6 min"
+      }
+    ]
   }
 ]
