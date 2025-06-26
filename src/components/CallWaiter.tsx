8906a8b6762b30c3733db728a5a762cd3fb5e0df
SDC <99742065+SdQL@users.noreply.github.com>
2025-06-25 22:16:39 -0500

    refactor: clean up CallWaiter component structure and improve readability

--- src/components/CallWaiter.tsx
+++ src/components/CallWaiter.tsx
@@ -1,28 +1,46 @@
-import React, {useState} from 'react'
-import { Button } from '@/components/ui/button';
-import { BellRing } from 'lucide-react';
-import Swal from 'sweetalert2'
-
-
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { BellRing } from "lucide-react";
+import Swal from "sweetalert2";
 
 const CallWaiter = () => {
-  const handleModal = () => {
-        Swal.fire({
-          icon: 'success',
-          title: 'Has llamado a un mesero',
-          text: 'Un mesero te atenderá pronto',
-          timer: 2500,
-          showConfirmButton: false
-        }); 
-  }
+  const [isDisabled, setIsDisabled] = useState(false);
 
+  const handleModal = () => {
+    setIsDisabled(true)
+
+    Swal.fire({
+      icon: "success",
+      title: "Has llamado a un mesero",
+      text: "Un mesero te atenderá pronto",
+      timer: 2500,
+      showConfirmButton: false,
+    });
+
+    setTimeout(() => {
+      setIsDisabled(false);
+    }, 600000)
+  };
 
   return (
-      <div className='flex fixed right-3 bottom-3 z-[999] justify-center items-center bg-amber-500 pl-6 py-1 rounded-xl pr-3 hover:bg-amber-400 hover:cursor-pointer'>
-        <BellRing className='bell'/>
-        <Button onClick={handleModal} variant='ghost' className='text-[16px] font-semibold hover:bg-transparent'>Listo para ordenar</Button>
-      </div>
-  )
-}
+    <div
+      className={
+        !isDisabled
+          ? "flex fixed right-3 bottom-3 justify-center items-center bg-amber-500 pl-6 py-1 rounded-xl pr-3 hover:bg-amber-400 hover:cursor-pointer}"
+          : "bg-amber-500/50 pointer-events-none flex fixed right-3 bottom-3 justify-center items-center pl-6 py-1 rounded-xl pr-3"
+      }
+    >
+      <BellRing className={!isDisabled ? "bell" : "opacity-50"} />
+      <Button
+        onClick={handleModal}
+        disabled={isDisabled}
+        variant="ghost"
+        className="text-[16px] font-semibold hover:bg-transparent"
+      >
+        {!isDisabled ? "Listo para ordenar" : "Ya llamaste a un mesero"}
+      </Button>
+    </div>
+  );
+};
 
-export default CallWaiter
+export default CallWaiter;
