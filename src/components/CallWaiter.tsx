c0c08fd2cb8c6e4fd749414ca60afa5e43fa6502
SDC <99742065+SdQL@users.noreply.github.com>
2025-07-02 00:46:56 -0500

    feat: implement countdown timer in CallWaiter component

--- src/components/CallWaiter.tsx
+++ src/components/CallWaiter.tsx
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { BellRing } from "lucide-react";
+import Countdown from "react-countdown";
 import Swal from "sweetalert2";
 
 const CallWaiter = () => {
   const [isDisabled, setIsDisabled] = useState(false);
+  const [expiryTime, setExpiryTime] = useState<Date | null>(null);
+
+  const renderer = ({ minutes, seconds }: { minutes: number; seconds: number; }) => {
+    return (
+      <span>
+        {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+      </span>
+    );
+  };
 
   const handleModal = () => {
-    setIsDisabled(true)
+    const time = new Date(Date.now() + 4 * 60 * 1000); // 4 minutos
+    setIsDisabled(true);
+    setExpiryTime(time);
 
     Swal.fire({
       icon: "success",
@@ -16,28 +28,36 @@ const CallWaiter = () => {
       timer: 2500,
       showConfirmButton: false,
     });
-
-    setTimeout(() => {
-      setIsDisabled(false);
-    }, 200000) // 
   };
 
   return (
     <div
-      className={
-        !isDisabled
-          ? "flex fixed right-3 bottom-3 justify-center items-center bg-amber-500 pl-6 py-1 rounded-xl pr-3 hover:bg-amber-400 hover:cursor-pointer}"
-          : "bg-amber-500/50 pointer-events-none flex fixed right-3 bottom-3 justify-center items-center pl-6 py-1 rounded-xl pr-3"
-      }
+      className={`fixed right-3 bottom-3 flex justify-center items-center pl-6 py-1 rounded-xl pr-3 transition ${
+        isDisabled
+          ? "bg-amber-500/50 pointer-events-none"
+          : "bg-amber-500 hover:bg-amber-400 cursor-pointer"
+      }`}
     >
-      <BellRing className={!isDisabled ? "bell" : "opacity-50"} />
+      <BellRing className={isDisabled ? "opacity-50" : "bell"} />
       <Button
         onClick={handleModal}
         disabled={isDisabled}
         variant="ghost"
         className="text-[16px] font-semibold hover:bg-transparent"
       >
-        {!isDisabled ? "Listo para ordenar" : "Ya llamaste a un mesero"}
+        {!isDisabled ? (
+          "Listo para ordenar"
+          
+        ) : expiryTime ? (
+          <Countdown
+            date={expiryTime}
+            renderer={renderer}
+            onComplete={() => {
+              setIsDisabled(false);
+              setExpiryTime(null);
+            }}
+          />
+        ) : null}
       </Button>
     </div>
   );
